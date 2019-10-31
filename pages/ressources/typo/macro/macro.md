71c2480bd338ac4a31740aa7dfa5af93dbaad751
Julien Bidoret <julienbidoret@gmail.com>
2019-10-31 02:36:23 +0100

    Macro

--- pages/ressources/typo/macro/macro.md
+++ pages/ressources/typo/macro/macro.md
@@ -1,1 +1,26 @@
 # Macrotypographie
+
+> La macrotypographie désigne l’agencement de l’espace typographique et la mise en page : le format de l’imprimé, la taille et la disposition des colonnes de texte et des illustrations, la hiérarchisation des titres et des légendes.  
+    — Le détail en typographie, Jost Hochuli, Éditions B42
+
+## Espace
+
+## Sélection
+
+## Rythme
+
+## Contraste et couleur
+
+Gris typographique
+
+## Structure, échelle et hiérarchie
+
+## Composition
+
+Défendre la veuve et l’orphelin, éviter les veuves et les orphelines.
+```
+p {
+    orphans: 2;
+    widows: 2;
+}
+```
--- snippets/ressources/type.php
+++ snippets/ressources/type.php
@@ -3,7 +3,7 @@
         <li class="<?= $subsubsection == 'webfonts' ? 'opened' : '' ?>"><a href="/web/pages/ressources/typo/">Webfonts</a></li>
     </ul>
     <ul>
-        <li class="<?= $subsubsection == 'start' ? 'opened' : '' ?>"><a href="/web/pages/ressources/typo/macro/">Macro</a> </li>
+        <li class="<?= $subsubsection == 'macro' ? 'opened' : '' ?>"><a href="/web/pages/ressources/typo/macro/">Macro</a> </li>
         <li class="<?= $subsubsection == 'micro' ? 'opened' : '' ?>"><a href="/web/pages/ressources/typo/micro/">Micro</a> </li>
         <li class="<?= $subsubsection == 'opentype' ? 'opened' : '' ?>"><a href="/web/pages/ressources/typo/opentype/">Opentype</a> </li>
         <li class="<?= $subsubsection == 'variables' ? 'opened' : '' ?>"><a href="/web/pages/ressources/typo/variables/">Variables</a> </li>
