4d97580c23759641b71571d07c979d46ad8d7682
Julien Bidoret <julienbidoret@gmail.com>
2019-10-31 02:37:00 +0100

    References Zones

--- pages/projets/zones/index.php
+++ pages/projets/zones/index.php
@@ -1,41 +1,28 @@
 <?php
-  include($_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php');
-  $Parsedown = new Parsedown();
+    // config
+    $title = "ÉSAD·Pyrénées — Ateliers web — Projets";
+    $section="projets";
+    $subsection="zones";
+    // $nav = "/web/snippets/ressources/NAV.php"; // specific subnav
+    $mdfile = "./zones.md";
 
-  $title = "ÉSAD·Pyrénées — Ateliers web — Projets";
-  $section="projets";
-  $subsection="zones";
-  include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php");
-  include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php");
+    // includes
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtra.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php";
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php";
+
+    // nav snippet
+    if(isset($nav)) include_once $_SERVER["DOCUMENT_ROOT"] . $nav;
+
+    // markdown!
+    $Parsedown = new ParsedownExtra();
+
 ?>
 
-<main class="pane active" id="content">
-    <h1>Zones</h1>
-
-    <p>
-    <a href="https://www.editions-zones.fr/">Zones</a> est une collection de livres de science humaines et cultural studies créée en 2007, et dirigée depuis par Grégoire Chamayou pour les Éditions de la Découverte. Le design des couvertures et de la collection est assuré par le studio De Valence. La plupart des livres sont publiés sur le principe du Lyber, créé par les <a href="http://www.lyber-eclat.net/lyber/lybertxt.html">Éditions de l’éclat</a> et sont pleinement lisibles en ligne.
-    </p>
-    <ol>
-        <li>Choisir un livre *</li>
-        <li>Choisir un ou quelques chapitres</li>
-        <li>Déterminer une iconographie / modalité d’illustration</li>
-        <li>Les mettre en page sous une forme web</li>
-        <li>Penser à une “couverture”, à la navigation…</li>
-        <li>Produire une feuille de style pour impression domestique</li>
-    </ol>
-
-    <p>
-        * Les livres :
-    </p>
-    <ul>
-        <li><a href="https://www.editions-zones.fr/livres/jouir/">Jouir, en quête de l’orgasme féminin</a>, de Sarah Barmak</li>
-        <li><a href="https://www.editions-zones.fr/livres/sorcieres/">Sorcières, la puissance invaincue des femmes</a>, de Mona Chollet</li>
-        <li><a href="https://www.editions-zones.fr/livres/propaganda/">Propaganda, Comment manipuler l'opinion en démocratie</a>, d’Edward Bernays (1928)</li>
-    </ul>
-    <p>
-        Le projet est de comprendre et d’appliquer les principes de mise en page du web contemporain, et d’appréhender la “fluidité” du texte dans son rapport à différents médias.
-    </p>    <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/date.php"); ?>
+    <main class="pane active" id="content">
+        <?= $Parsedown->text( file_get_contents( $mdfile ) ); ?>
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/date.php"); ?>
     </main>
-<?php
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php");
-?>
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php"); ?>
---
+++ pages/projets/zones/zones.md
@@ -0,0 +1,57 @@
+
+# Zones
+
+[Zones](https://www.editions-zones.fr/) est une collection de livres de science humaines et cultural studies créée en 2007, et dirigée depuis par Grégoire Chamayou pour les Éditions de la Découverte. Le design des couvertures et de la collection est assuré par le studio De Valence. La plupart des livres sont publiés sur le principe du Lyber, créé par les [Éditions de l’éclat](http://www.lyber-eclat.net/lyber/lybertxt.html) et sont pleinement lisibles en ligne.
+
+1.  Choisir un livre *
+2.  Choisir un ou quelques chapitres
+3.  Déterminer une iconographie / modalité d’illustration
+4.  Les mettre en page sous une forme web
+5.  Penser à une “couverture”, à la navigation…
+6.  Produire une feuille de style pour impression domestique
+
+\* Les livres (2019) :
+
+*   [Jouir, en quête de l’orgasme féminin](https://www.editions-zones.fr/livres/jouir/), de Sarah Barmak
+*   [Sorcières, la puissance invaincue des femmes](https://www.editions-zones.fr/livres/sorcieres/), de Mona Chollet
+*   [Propaganda, Comment manipuler l'opinion en démocratie](https://www.editions-zones.fr/livres/propaganda/), d’Edward Bernays (1928)
+
+Le projet est de comprendre et d’appliquer les principes de mise en page du web contemporain, et d’appréhender la “fluidité” du texte dans son rapport à différents médias.
+
+
+### Références internes
+
+- [Typographie & numérique](../../references/typo/)
+- [Fonderies](../../references/foundries/)
+
+### Ressources internes
+
+- [HTML / Contenu](../../ressources/html/content/)
+- [Typo & web](../../ressources/typo/)
+- [CSS / Texte](../../ressources/css/text/)
+
+### Lectures et regards
+
+- [Font Review Journal](https://fontreviewjournal.com/)
+- [The Shape of Design](https://shapeofdesignbook.com/)
+- [Why Do All Websites Look the Same?](https://modus.medium.com/on-the-visual-weariness-of-the-web-8af1c969ce73)
+- [Anti web design Manifesto](http://brandon.invergo.net/news/2013-03-10-Anti-web-design-Manifesto.html)
+- [Motherfucking website](http://motherfuckingwebsite.com/)
+- [Still a motherfucking website](http://bettermotherfuckingwebsite.com/)
+- [The public school for architecture brussels](http://psfa-bxl.org/)
+- [Koenig & Clinton](http://koenigandclinton.com/gallery/)
+- [Common Room](http://common-room.net/)
+- [Your world of text](https://www.yourworldoftext.com/)
+- [DIA](https://dia.tv/)
+- [Editorial New](https://editorialnew.com/)
+- [L’ebook redistribuable](https://jaypanoz.github.io/reflow/)
+
+
+### Textes
+<ul>
+<li><a download href="texts/attitude.txt">László Moholy-Nagy</a></li>
+<li><a download href="texts/bartleby.txt">Bartleby</a></li>
+<li><a download href="texts/lessing_freeculture.odt">Free Culture</a></li>
+<li><a download href="texts/emigre.pdf">Emigre</a> (<a href="texts/emigre.odt">texte</a>)</li>
+
+</ul>
