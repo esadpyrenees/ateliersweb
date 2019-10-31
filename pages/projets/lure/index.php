e32a1149e7f0a3f3eaee2aecbf0200cb827b31ea
Julien Bidoret <julienbidoret@gmail.com>
2019-10-31 02:35:32 +0100

    Lure

---
+++ pages/projets/lure/index.php
@@ -0,0 +1,28 @@
+<?php
+    // config
+    $title = "ÉSAD·Pyrénées — Ateliers web — Ressources";
+    $section = "projets";
+    $subsection = "typo";
+    // $nav = "/web/snippets/ressources/type.php"; // specific subnav
+    $mdfile = "./lure.md";
+
+    // includes
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtra.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php";
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php";
+
+    // nav snippet
+    if(isset($nav)) include_once $_SERVER["DOCUMENT_ROOT"] . $nav;
+
+    // markdown!
+    $Parsedown = new ParsedownExtra();
+
+?>
+
+    <main class="pane active" id="content">
+        <?= $Parsedown->text( file_get_contents( $mdfile ) ); ?>
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/date.php"); ?>
+    </main>
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php"); ?>
