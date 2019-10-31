d20c643e935a0ce946046d789cc6135db3b67eac
Julien Bidoret <julienbidoret@gmail.com>
2019-10-31 02:36:45 +0100

    Foundries in md

---
+++ pages/references/foundries/foundries.md
@@ -0,0 +1,72 @@
+# Fonderies numériques
+
+
+
+## Libre et open-source
+
+Les fontes distribuées sous licence libres sont nombreuses et leur qualité augmente constamment. La liste ci-dessous est empruntée à [Franck Adebiaye](http://fadebiaye.com/), qui maintient un [annuaire de la scène typographique française](http://www.fadebiaye.com/type/1554/typo_scene_fr.html).
+
+*   [Velvetyne Type Foundry](http://velvetyne.fr/)
+*   [OpenSourcePublishing Foundry](http://ospublish.constantvzw.org/foundry/)
+*   [Open Font Library](http://openfontlibrary.org/)
+*   [The League of Moveable Type](http://www.theleagueofmoveabletype.com/)
+*   [Omnibus Type](http://www.omnibus-type.com/)
+*   [Open Foundry](https://open-foundry.com/)
+
+Raphaël Bastide a initié et développé un outil (libre) pour lister et sélectionner des fontes numériques. Il contient une liste importante de polices libres ou open source : [use & modify](http://usemodify.com/).
+
+Raphaël Bastide maintient également une liste de fonderies libres : [gitlab.com/raphaelbastide/libre-foundries](https://gitlab.com/raphaelbastide/libre-foundries)
+
+Google est également devenu un distributeur de fontes ayant à son catalogue de nombreuses polices de qualité (pas toutes…) : [Google Web Fonts](http://www.google.com/webfonts). <span class="blink">On se rappellera néanmoins qu’avec Google, rien n’est _vraiment_ gratuit. Google se sert en effet des liens d’intégration des fontes comme de _trackers_ lui permettant d’accumuler des données sur les visiteurs de votre page web</span>.
+
+[Font Squirrel](http://www.fontsquirrel.com/) propose de nombreuses polices libres et/ou gratuites et dispose d’un excellent générateur de [« packs » @font-face](http://www.fontsquirrel.com/fontface/generator).
+
+## Services et fonderies commerciales
+
+La plupart des fonderies de caractères a aujourd’hui pris le virage de la typographie pour le web. Les conditions d’accès aux webfonts sont cependant très différentes d’une fonderie à l’autre. La plupart des sites spécialisés offrent la mise à disposition de webfonts contre un abonnement annuel ; c’est le cas des gros diffuseurs cités ci-dessous. Ils ne doivent cependant pas éclipser la multiplicité des possibilités offertes par les diffuseurs et fonderies indépendantes.
+
+#### Gros diffuseurs
+
+*   [Typekit](http://typekit.com/) (devenu [Adobe fonts](https://fonts.adobe.com/))
+*   [Monotype](http://fonts.com)
+*   [Myfonts](http://myfonts.com)
+*   [Fontshop](http://fontshop.com)
+*   [Hoefler & co.](https://www.typography.com/)
+
+
+## Quelques indépendants à connaître
+
+*   [James Edmonson – OhNo Type Co](https://ohnotype.co/)
+*   \+ [futurefonts.xyz](https://www.futurefonts.xyz/)
+*   [David Jonathan Ross](https://djr.com/)
+*   \+ [Font of the Month Club](https://fontofthemonth.club/)
+*   [Village](http://vllg.com/)
+*   [Binnenland](http://www.binnenland.ch)
+*   [Colophon Foundry](http://www.colophon-foundry.org)
+*   [Commercial Type](http://commercialtype.com)
+*   [Fontbureau](http://www.fontbureau.com)
+*   [Jérémy Landes](http://studiotriple.fr/)
+*   [Fontsmith](http://www.fontsmith.com)
+*   [House Industries](http://houseindustries.com)
+*   [Lineto](http://lineto.com)
+*   [Optimo](http://www.optimo.ch)
+*   [Ourtype](http://ourtype.be)
+*   [Playtype](https://playtype.com/)
+*   [Smeltery](http://smeltery.net)
+*   [Typotheque](http://www.typotheque.com)
+*   [Underware](http://www.underware.nl)
+*   [Benoît Bodhuin](https://www.bb-bureau.fr/)
+*   [Grilli](https://www.grillitype.com/)
+*   [Production Type](https://www.productiontype.com/)
+*   [Lucasdescroix](http://www.lucasdescroix.fr/)
+
+## Twitter
+*   [#](https://twitter.com/)
+
+## Blogs et ressources
+
+*   [I love typography](http://ilovetypography.com/)
+*   [Font Review Journal](http://fontreviewjournal.com/)
+*   [Typewolf](http://www.typewolf.com/), notamment [typewolf.com/resources](https://www.typewolf.com/resources)
+*   [Ministry Of Type](http://ministryoftype.co.uk/) (almost dead)
+*   [Typographica](http://typographica.org/) (almost dead)
--- pages/references/foundries/index.php
+++ pages/references/foundries/index.php
@@ -1,97 +1,27 @@
-  <?php
-    $title = "ÉSAD·Pyrénées — Ateliers web — Références";
-    $section="references";
-    $subsection="foundries";
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php");
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php");
-  ?>
+<?php
+    // config
+    $title = "ÉSAD·Pyrénées — Ateliers web — Ressources";
+    $section = "references";
+    $subsection = "foundries";
+    $mdfile = "./foundries.md";
 
-  <main class="pane active" id="content">
-    <h1>Fonderies numériques</h1>
-    <section class="section">
+    // includes
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtra.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php";
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php";
 
+    // nav snippet
+    // include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/ressources/type.php";
 
+    // markdown!
+    $Parsedown = new ParsedownExtra();
 
-            <h2 class="section-subtitle">Libre et open-source</h2>
-            <p>
-                Les fontes distribuées sous licence libres sont nombreuses et leur qualité augmente constamment.
-                La liste ci-dessous est empruntée à <a href="http://fadebiaye.com/">Franck Adebiaye</a>, qui maintient un <a href="http://www.fadebiaye.com/type/1554/typo_scene_fr.html">annuaire de la scène typographique française</a>.
-            </p>
-            <ul>
-                <li><a href="http://velvetyne.fr/"> Velvetyne Type Foundry</a></li>
-                <li><a href="http://ospublish.constantvzw.org/foundry/">OpenSourcePublishing Foundry</a></li>
-                <li><a href="http://openfontlibrary.org/">Open Font Library</a></li>
-                <li><a href="http://www.theleagueofmoveabletype.com/">The League of Moveable Type</a></li>
-                <li><a href="http://www.omnibus-type.com/">Omnibus Type</a></li>
-                <li><a href="https://open-foundry.com/"> Open Foundry</a></li>
-            </ul>
+?>
 
-            <p>
-                Raphaël Bastide a initié et développé un outil (libre) pour lister et sélectionner des fontes numériques.
-                Il contient une liste importante de polices libres ou open source :
-                <a href="http://usemodify.com/">use &amp; modify</a>.
-            </p>
-            <p>
-                Raphaël Bastide maintient également une liste de fonderies libres : <a href="https://gitlab.com/raphaelbastide/libre-foundries">gitlab.com/raphaelbastide/libre-foundries</a>
-            </p>
-            <p>
-                Google est également devenu un distributeur de fontes ayant à son catalogue de nombreuses polices de qualité (pas toutes…) : <a href="http://www.google.com/webfonts">Google Web Fonts</a>. <span class="blink">On se rappellera néanmoins qu’avec Google, rien n’est <em>vraiment</em> gratuit. Google se sert en effet des liens d’intégration des fontes comme de <i>trackers</i> lui permettant d’accumuler des données sur les visiteurs de votre page web</span>.
-            </p>
-            <p><a href="http://www.fontsquirrel.com/">Font Squirrel</a> propose de nombreuses polices libres et/ou gratuites et dispose d’un excellent générateur de <a href="http://www.fontsquirrel.com/fontface/generator">«&nbsp;packs&nbsp;» @font-face</a>.</p>
-
-
-            <h2 class="section-subtitle">Services et fonderies commerciales</h2>
-            <p>
-                La plupart des fonderies de caractères a aujourd’hui pris le virage de la typographie pour le web. Les conditions d’accès aux webfonts sont cependant très différentes d’une fonderie à l’autre.
-                La plupart des sites spécialisés offrent la mise à disposition de webfonts contre un abonnement annuel ; c’est le cas des gros diffuseurs cités ci-dessous.
-                Ils ne doivent cependant pas éclipser la multiplicité des possibilités offertes par les diffuseurs et fonderies indépendantes.
-            </p>
-            <h4>Gros diffuseurs</h4>
-            <ul>
-                <li><a href="http://typekit.com/">Typekit</a></li>
-                <li><a href="http://fonts.com">Monotype</a></li>
-                <li><a href="http://myfonts.com">Myfonts</a></li>
-                <li><a href="http://fontshop.com">Fontshop</a></li>
-                <li><a href="https://www.typography.com/">Hoefler &amp; co.</a></li>
-            </ul>
-            <h4>Quelques indépendants à connaître</h4>
-            <ul>
-                <li><a href="http://vllg.com/" title="">Village</a></li>
-                <li><a href="http://www.binnenland.ch">Binnenland</a></li>
-                <li><a href="http://www.colophon-foundry.org">Colophon Foundry            </a></li>
-                <li><a href="http://commercialtype.com">Commercial Type           </a></li>
-                <li><a href="http://www.fontbureau.com">Fontbureau           </a></li>
-                <li><a href="http://www.fontsmith.com">Fontsmith</a></li>
-                <li><a href="http://houseindustries.com" >House Industries</a></li>
-                <li><a href="http://lineto.com">Lineto</a></li>
-                <li><a href="http://www.optimo.ch">Optimo</a></li>
-                <li><a href="http://ourtype.be">Ourtype</a></li>
-                <li><a href="https://playtype.com/">Playtype</a></li>
-                <li><a href="http://smeltery.net">Smeltery</a></li>
-                <li><a href="http://suitcasetype.com">Suitcase Type</a></li>
-                <li><a href="http://www.typotheque.com">Typotheque</a></li>
-                <li><a href="http://www.underware.nl">Underware            </a></li>
-                <li><a href="https://www.grillitype.com/">Grilli         </a></li>
-                <li><a href="https://www.productiontype.com/">Production Type </a></li>
-            </ul>
-
-            <h2 class="section-subtitle">Blogs et ressources</h2>
-            <ul>
-                <li><a href="http://ilovetypography.com/" title="">I love typography</a></li>
-                <li><a href="http://fontreviewjournal.com/">Font Review Journal</a> </li>
-                <li><a href="http://www.typewolf.com/" title="">Typewolf</a>, notamment <a href="https://www.typewolf.com/resources">typewolf.com/resources</a>   </li>
-                <li><a href="http://ministryoftype.co.uk/">Ministry Of Type</a> (almost dead)</li>
-                <li><a href="http://typographica.org/">Typographica</a> (almost dead)</li>
-            </ul>
-
-
-
-
-            </section>
-
+    <main class="pane active" id="content">
+        <?= $Parsedown->text( file_get_contents( $mdfile ) ); ?>
         <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/date.php"); ?>
     </main>
 
-  <?php
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php");
-  ?>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php"); ?>
