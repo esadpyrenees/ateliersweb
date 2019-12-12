2720c4bb5e9723bdea5990f6fbc3422904afe9bf
Julien Bidoret <julienbidoret@gmail.com>
2019-12-12 20:56:24 +0100

    Super weird include attempt (php include)

---
+++ snippets/examples_nav.php
@@ -0,0 +1,7 @@
+<style media="screen">
+    #embednav {font-family:'Ecole',sans-serif; position: fixed; top: 0; left:0;}
+    #embednav a{min-width: 1.15em;display: inline-block;color:#fff;font-size:.85em;background:#000;text-transform:uppercase;text-decoration:none;border:none;padding:.6em .5em .25em;}#embednav a span{display:none}#embednav a:hover span{display:inline}
+</style>
+<nav id="embednav" >
+    <a href="../">↩ <span>retour</span></a><br><a class='download' download href='../backup.php?dir=<?php echo str_replace("/web/pages/exemples/", "", $_SERVER['REQUEST_URI']) ?>'>☻ <span>télécharger</span></a>
+</nav>
