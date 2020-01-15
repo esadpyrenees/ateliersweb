69640badcd7c333fe2d239ad2f80222bab2ccc9c
Julien Bidoret <julienbidoret@gmail.com>
2020-01-15 01:02:19 +0100

    Solidification embedded nav

--- snippets/examples_nav.php
+++ snippets/examples_nav.php
@@ -1,7 +1,8 @@
-<style media="screen">
-    #embednav {font-family:'Ecole',sans-serif; position: fixed; top: 0; left:0;}
-    #embednav a{min-width: 1.15em;display: inline-block;color:#fff;font-size:.85em;background:#000;text-transform:uppercase;text-decoration:none;border:none;padding:.6em .5em .25em;}#embednav a span{display:none}#embednav a:hover span{display:inline}
+<style media="all">
+    #embednav {font-family:'Ecole',sans-serif; position: fixed; top: 0; left:0; font-size: 16px; display: flex; align-items: flex-end;; line-height:24px; background: none; border:none; padding: 0; margin:0}
+    #embednav a{min-width: 1.15em;display: block; height:24px; line-height: 27px; color:#fff;font-size:.85em;background:#000;text-transform:uppercase;text-decoration:none;border:none;padding:0 .5em;}#embednav a span{display:none}#embednav a:hover span{display:inline}
+    @media print {#embednav {display: none; }}
 </style>
 <nav id="embednav" >
-    <a href="../">↩ <span>retour</span></a><br><a class='download' download href='../backup.php?dir=<?php echo str_replace("/web/pages/exemples/", "", $_SERVER['REQUEST_URI']) ?>'>☻ <span>télécharger</span></a>
+    <a href="../">↩ <span>retour</span></a><a class='download' download href='../backup.php?dir=<?php echo str_replace("/web/pages/exemples/", "", $_SERVER['REQUEST_URI']) ?>'>☻ <span>télécharger</span></a>
 </nav>
