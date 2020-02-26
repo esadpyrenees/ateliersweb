bb54230e1c065a5857437cc09cd3047eaa03d440
Julien Bidoret <julienbidoret@gmail.com>
2020-02-26 11:54:08 +0100

    Update tools

--- pages/outils/index.php
+++ pages/outils/index.php
@@ -1,66 +1,27 @@
-  <?php
-    $title = "ÉSAD·Pyrénées — Ateliers web — Exemples";
+<?php
+    // config
+    $title = "Outils";
     $section="outils";
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php");
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php");
-  ?>
+    $mdfile = "./outils.md";
 
-  <main class="pane active" id="content">
-    <h1 class="section-title">Outils</h1>
-    <section id="editeurs">
-    <h2 class="section-subtitle">Éditeurs</h2>
-    <ul>
-        <li><a href="http://atom.io">Atom (open source, Github)</a></li>
-        <li><a href="http://brackets.io">Brackets (open source, Adobe)</a></li>
-        <li><a href="https://code.visualstudio.com/">Visual studio Code (open source, Microsoft)</a>   </li>
-        <li><a href="http://www.sublimetext.com">Sublime Text ($)</a></li>
-    </ul>
-    </section>
-    <section id="servers">
-    <h2 class="section-subtitle">Serveurs de développement</h2>
-    <ul>
-        <li><a href="http://mamp.info/">MAMP</a></li>
-        <li><a href="https://www.apachefriends.org/">XAMPP</a></li>
-    </ul>
-    </section>
-    <section id="cms">
+    // includes
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtra.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php";
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php";
 
-        <h2 class="section-subtitle">Gestionnaires de contenus</h2>
-        <p>Gestionnaires de contenus sans base de données.</p>
-        <ul>
-            <li><a href="http://staceyapp.com">Stacey</a></li>
-            <li><a href="http://www.berta.me/download/">Berta</a></li>
-            <li><a href="http://getkirby.com">Kirby ($)</a></li>
-            <li><a href="http://subfolio.com">Subfolio</a></li>
-        </ul>
-        <p>Gestionnaires de contenus avec base de données.</p>
-        <ul>
-            <li><a href="http://wordpress.org">Wordpress</a></li>
-            <li><a href="http://spip.net/">Spip</a></li>
-            <li><a href="http://indeshibit.org">Indexhibit ($)</a></li>
-        </ul>
+    // nav snippet
+    if(isset($nav)) include_once $_SERVER["DOCUMENT_ROOT"] . $nav;
 
-        <p>Services web.</p>
-        <ul>
-            <li><a href="http://wordpress.com">Wordpress.com</a></li>
-            <li><a href="http://tumblr.com">Tumblr</a></li>
-            <li><a href="http://cargocollective.com/">Cargo ($)</a></li>
-            <li><a href="http://squarespace.com/">Squarespace ($)</a></li>
-        </ul>
+    // markdown!
+    $Parsedown = new ParsedownExtra();
 
-    </section>
+?>
 
-    <section>
-        <h2>Kirby</h2>
-        <ul>
-            <li><a href="https://gekirby.com">Kirby</a></li>
-            <li><a href="https://getkirby.com/docs/cookbook/setup/kirby-in-a-nutshell">Kirby in a nutshell</a></li>
-        </ul>
-    </section>
-
-      <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/date.php"); ?>
+    <main class="pane active" id="content">
+        <?= $Parsedown->text( file_get_contents( $mdfile ) ); ?>
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/date.php"); ?>
     </main>
 
-  <?php
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php");
-  ?>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php"); ?>
+
---
+++ pages/outils/outils.md
@@ -0,0 +1,47 @@
+# Outils
+
+### Éditeurs
+
+*   [Visual studio Code (open source, Microsoft)](https://code.visualstudio.com/)
+*   [Atom (open source, Github)](http://atom.io)
+*   [Brackets (open source, Adobe)](http://brackets.io)
+
+### Serveurs de développement
+
+*   [MAMP](http://mamp.info/)
+*   [XAMPP](https://www.apachefriends.org/)
+
+### Outils génératifs
+* [Processing](http://processing.org/)
+* [OpenFrameworks](https://openframeworks.cc/)
+* [Drawbot](https://drawbot.com/) (python, typo, variable fonts)
+* [p5Stamper](https://p5stamper.com/) (javascript, processing)
+
+### Gestionnaires de contenus
+
+Gestionnaires de contenus sans base de données.
+
+*   [Stacey](http://staceyapp.com)
+*   [Berta](http://www.berta.me/download/)
+*   [Kirby ($)](http://getkirby.com)
+*   [Subfolio](http://subfolio.com)
+
+Gestionnaires de contenus avec base de données.
+
+*   [Wordpress](http://wordpress.org)
+*   [Spip](http://spip.net/)
+*   [Indexhibit ($)](http://indeshibit.org)
+
+Services web.
+
+*   [Wordpress.com](http://wordpress.com)
+*   [Tumblr](http://tumblr.com)
+*   [Cargo ($)](http://cargocollective.com/)
+*   [Squarespace ($)](http://squarespace.com/)
+
+
+### Kirby
+
+*   [Kirby](https://gekirby.com)
+*   [Kirby in a nutshell](https://getkirby.com/docs/cookbook/setup/kirby-in-a-nutshell)
+</main>
