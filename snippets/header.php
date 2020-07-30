1eb1fd626a8bc16e2fc09ddd45598c52ad913d25
Julien Bidoret <julienbidoret@Max.local>
2020-07-30 12:30:41 +0200

    Use of undefined constant HTTP_HOST

--- snippets/header.php
+++ snippets/header.php
@@ -6,8 +6,8 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <?php
     $title_line = trim($title);
-    $host = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$_SERVER[HTTP_HOST];
-    $localurl = $host . $_SERVER[REQUEST_URI];
+    $host = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$_SERVER['HTTP_HOST'];
+    $localurl = $host . $_SERVER['REQUEST_URI'];
     echo "<title>ÉSAD Pyrénées – Ateliers web – $title_line</title>\n\n";
     echo "        <meta property='og:title' content='ÉSAD Pyrénées – Ateliers web – $title_line'>\n";
     echo "        <meta property='og:description' content='Ressources, références et exemples des ateliers web de l’École supérieure d’art et de design des Pyrénées, réunies et proposées par Julien Bidoret.'>\n";
