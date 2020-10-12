04d5c0651223809779dcc613419a516fd9525f10
Julien Bidoret – accentgrave <julien@accentgrave.net>
2020-10-12 22:07:09 +0200

    Mise à jour de la nav

--- snippets/nav.php
+++ snippets/nav.php
@@ -53,7 +53,9 @@
         <li class="<?php echo $subsection == 'typo' ? 'opened' : '' ?>"><a href="/web/pages/references/typo/">Typographie</a></li>
         <li class="<?php echo $subsection == 'foundries' ? 'opened' : '' ?>"><a href="/web/pages/references/foundries/">Fonderies</a></li>
         <li class="<?php echo $subsection == 'ecrituresnumeriques' ? 'opened' : '' ?>"><a href="/web/pages/references/ecrituresnumeriques/">Écritures numériques</a></li>
-        <li class="<?php echo $subsection == 'histoire' ? 'opened' : '' ?>"><a href="/web/pages/references/histoire/">Histoire du web</a></li>
+        <li class="<?php echo $subsection == 'webdocumentaire' ? 'opened' : '' ?>"><a href="/web/pages/references/webdocumentaire/">Webdoc</a></li>
+        <li class="<?php echo $subsection == 'visite' ? 'opened' : '' ?>"><a href="/web/pages/references/visite/">Visite</a></li>        
+        <li class="<?php echo $subsection == 'histoire' ? 'opened' : '' ?>"><a href="/web/pages/references/histoire/">Histoire</a></li>
         <li class="<?php echo $subsection == 'gopro' ? 'opened' : '' ?>"><a href="/web/pages/references/gopro/">Go pro !</a></li>
     </ul>
 </nav>
---
+++ snippets/references/histoire.php
@@ -0,0 +1,10 @@
+<nav class="pane active">
+    <ul>
+        <li class="<?= $subsubsection == 'intro' ? 'opened' : '' ?>"><a href="/web/pages/references/histoire/">Introduction</a></li>
+    </ul>
+
+    <ul>
+        <li class="<?= $subsubsection == 'internet' ? 'opened' : '' ?>"><a href="/web/pages/references/histoire/internet/">Internet</a></li>
+        <li class="<?= $subsubsection == 'machines' ? 'opened' : '' ?>"><a href="/web/pages/references/histoire/machines/">Machines</a></li>
+    </ul>
+  </nav>
