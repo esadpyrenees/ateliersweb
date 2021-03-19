a24d7e855e510f78748664413da84b858783031a
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-03-19 14:51:47 +0100

    Webring

--- assets/js/webring.js
+++ assets/js/webring.js
@@ -1,15 +1,27 @@
 var p = document.createElement('p'),
   head = document.head,
   style = document.createElement('style'),
-  css = '#webring { background: red; }',
+  css = '#webring { position: fixed; color: white; background: #000; bottom: 0; width: 100vw; left: 0; padding: 10px; display: flex; justify-content: space-between; } #webring a {flex: 1; color: white; text-decoration: none; font-size: 16px; font-family: sans-serif; text-align: center;}',
   links = [
-    "https://bylerida.alwaysdata.net/audio/",
-    "https://bonnemazousarah.alwaysdata.net/audio/",
-    "https://bosschaerts.alwaysdata.net/audio/",
-    "https://anitaz.alwaysdata.net/audio/",
-    "https://andreascamps.alwaysdata.netaudio/",
-    "http://chbzdesign.alwaysdata.net/audio/",
-    "https://hannah.alwaysdata.net/audio/"
+    "https://amandine-l.alwaysdata.net/exercices/sons/",
+    "https://capucine.alwaysdata.net/microonde/",
+    "https://astridgautier.alwaysdata.net/exercices/ts.html",
+    "https://massinissa.alwaysdata.net/webring/webring.html",
+    "https://maevahuber.alwaysdata.net/video/index.html",
+    "https://merilin.alwaysdata.net/mavuidieo.html",
+    "https://alicemry.alwaysdata.net/video/video.html",
+    "https://andreas2camps.alwaysdata.net/son/son.html",
+    "https://bosschaerts.alwaysdata.net/ILOVEE MEU",
+    "https://guigui.alwaysdata.net/19_03_2021/",
+    "https://chbzdesign.alwaysdata.net/video/ring",
+    "https://lorelei-jougleux.alwaysdata.net/webring/tea.html",
+    "https://emmasdata.alwaysdata.net/",
+    "https://chrismaik.alwaysdata.net/video/",
+    "https://bonnemazousarah.alwaysdata.net/pages_web/webring/webring.html",
+    "https://alixlbrt.alwaysdata.net/audio/",
+    "https://morganne.alwaysdata.net/son/index.html",
+    "https://baptisteroca.alwaysdata.net/",
+    "https://jadelafont.alwaysdata.net/2021-03-19/",
   ];
 
   
@@ -18,19 +30,11 @@ var p = document.createElement('p'),
 
   for(var i = 0; i<links.length; i++){
     var a = document.createElement('a');
-    a.textContent = i;
+    var firstletters = links[i].split('//')[1].substring(0,2);
+    a.textContent = firstletters;
     a.href = links[i];
     p.appendChild(a);
   }
 
   p.id= "webring";
   document.body.appendChild(p);
-// 	
-	
-// container.innerText = '';
-// a.setAttribute("href",links[Math.floor(Math.random() * links.length)]);
-// p.setAttribute("style", "text-align:center;");
-// img.setAttribute('alt', "THANK YOU FOR SHOPPING HERE YOUR BUSINESS IS APPRECIATED");
-// img.setAttribute('src', "http://newyorkcirculars.info/thankyouforshopping.png");
-// img.setAttribute('style', "border:3px solid #000;");
-// container.appendChild(p);
