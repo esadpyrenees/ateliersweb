73631d02ba963126da2b9a4874bae1d7e19de1bc
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-04-01 21:45:52 +0200

    Cultures num 2nd semestre, first take

---
+++ snippets/projets/culturenum.php
@@ -0,0 +1,7 @@
+<nav class="pane active">
+  <ul>
+    <li class="<?= $subsubsection == 'intro' ? 'opened' : '' ?>"><a href="/web/pages/projets/culturenum/">Introduction</a></li>
+    <li class="<?= $subsubsection == 'ethique' ? 'opened' : '' ?>"><a href="/web/pages/projets/culturenum/ethique">Design éthique</a></li>
+    <!-- <li class="<?= $subsubsection == 'assets' ? 'opened' : '' ?>"><a href="/web/pages/projets/culturenum/libre/">Design libre</a></li> -->
+  </ul>
+</nav>
