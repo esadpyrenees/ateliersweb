3a4d8ed2ab7a84316d333e2b53a1c13ec4d4dee7
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-12-03 08:41:43 +0100

    Réorganisation html2print

--- snippets/ressources/ctrlaltprint.php
+++ snippets/ressources/ctrlaltprint.php
@@ -2,7 +2,7 @@
     <ul>
         <li class="<?= $subsubsection == 'intro' ? 'opened' : '' ?>"><a href="/web/pages/ressources/ctrl-alt-print/">Introduction</a></li>    
         <li class="<?= $subsubsection == 'resources' ? 'opened' : '' ?>"><a href="/web/pages/ressources/ctrl-alt-print/resources/">Collectifs et initiatives</a></li>    
-        <li class="<?= $subsubsection == 'tools' ? 'opened' : '' ?>"><a href="/web/pages/ressources/ctrl-alt-print/tools/">Outils et projets</a></li>    
+        <li class="<?= $subsubsection == 'tools' ? 'opened' : '' ?>"><a href="/web/pages/ressources/ctrl-alt-print/tools/">Outils</a></li>    
         <li class="<?= $subsubsection == 'css2' ? 'opened' : '' ?>"><a href="/web/pages/ressources/ctrl-alt-print/css2/">CSS 2.0</a></li>
         <li class="<?= $subsubsection == 'pagedjs' ? 'opened' : '' ?>"><a href="/web/pages/ressources/ctrl-alt-print/pagedjs/">Paged.js</a></li>
     </ul>
