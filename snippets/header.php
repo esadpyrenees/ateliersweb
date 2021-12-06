ee6625ceff154762a2b612f21a79104ef0e13324
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-12-06 08:08:11 +0100

    Favicon links

--- snippets/header.php
+++ snippets/header.php
@@ -10,6 +10,7 @@
     $localurl = $host . $_SERVER['REQUEST_URI'];
     echo "<title>ÉSAD Pyrénées – Ateliers web – $title_line</title>\n\n";
     echo "        <meta property='og:title' content='ÉSAD Pyrénées – Ateliers web – $title_line'>\n";
+    echo "        <meta property='twitter:title' content='ÉSAD Pyrénées – Ateliers web – $title_line'>\n";
     echo "        <meta property='og:description' content='Ressources, références et exemples des ateliers web de l’École supérieure d’art et de design des Pyrénées, réunies et proposées par Julien Bidoret.'>\n";
     echo "        <meta property='og:url' content='$localurl'>\n";
     echo "        <meta property='og:type' content='website'>\n";
@@ -17,22 +18,33 @@
     echo "        <meta property='og:locale' content='fr'>\n";
     if(isset($ogp_url)){
     echo "        <meta property='og:image' content='$host/web/medias/$ogp_url'>\n";
+    echo "        <meta name='twitter:description' content='$host/web/medias/$ogp_url'>\n";
     } else {
-    echo "        <meta property='og:image' content='$host/web/medias/$section" . (isset($subsection) ? '/' . $subsection : "") . "/ogp.png'>\n";    
+      echo "        <meta property='og:image' content='$host/web/medias/$section" . (isset($subsection) ? '/' . $subsection : "") . "/ogp.png'>\n";    
+      echo "        <meta property='twitter:image' content='$host/web/medias/$section" . (isset($subsection) ? '/' . $subsection : "") . "/ogp.png'>\n";    
     }
     if(isset($canonical_url)){
       echo '<link rel="canonical" href="'. htmlspecialchars($canonical_url) .'" >';
-    }
-    
-
+    }   
   ?>
   <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:site" content="@esadpyrenees">
+  <meta name="twitter:site" content="@ateliers_web">
   <meta name="twitter:creator" content="@julienbidoret">
 
   <meta name="author" content="Julien Bidoret">
 
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/web/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/web/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/web/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/web/favicon/site.webmanifest">
+  <link rel="mask-icon" href="/web/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#2b5797">
+  <meta name="theme-color" content="#ffffff">
+
 
   <link rel="stylesheet" href="/web/assets/css/icons.css">
   <link rel="stylesheet" href="/web/assets/css/main.css?yep">
