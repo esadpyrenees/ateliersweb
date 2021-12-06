4a35d83144202dfb0082d07eaa407b22dcb06254
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-12-06 08:07:14 +0100

    No more jQuery

--- snippets/footer.php
+++ snippets/footer.php
@@ -1,7 +1,5 @@
 
   <script src="/web/assets/js/vendor/modernizr-3.6.0.min.js"></script>
-  <script src="/web/assets/js/vendor/jquery-3.3.1.min.js"></script>
-  <script src="/web/assets/js/plugins.js"></script>
   <script src="/web/assets/js/notes.js"></script>
   <script src="/web/assets/js/main.js"></script>
   <?php if(isset($custom_js)): ?>
