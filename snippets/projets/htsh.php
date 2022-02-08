68cf5d98e2475978125688c5ad68a349fbcf4025
Julien Bidoret – accentgrave <julien@accentgrave.net>
2022-02-08 15:39:47 +0100

    HTSH

--- snippets/projets/htsh.php
+++ snippets/projets/htsh.php
@@ -4,9 +4,7 @@
     <li><a href="/web/pages/projets/htsh/#hypertextualite">Hypertextualité</a></li>
     <li><a href="/web/pages/projets/htsh/#objectif">Objectif</a></li>
     <li><a href="/web/pages/projets/htsh/#contraintes">Contraintes</a></li>
-    <li><a href="/web/pages/projets/htsh/#methode">Méthode</a></li>
-    <li><a href="/web/pages/projets/htsh/#mediums">Mediums</a></li>
-    <li><a href="/web/pages/projets/htsh/#refs">Références</a></li>
-    <li><a href="/web/pages/projets/htsh/#tools">Outils</a></li>
+    <li><a href="/web/pages/projets/htsh/#references">Références</a></li>
+    <li><a href="/web/pages/projets/htsh/#outils">Outils</a></li>
   </ul>
 </nav>
