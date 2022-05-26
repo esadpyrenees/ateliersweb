119a2009dac69071aced702d821ce2070a0a1014
Julien Bidoret – accentgrave <julien@accentgrave.net>
2022-05-26 22:29:38 +0200

    Detects if archive dir has index

--- pages/archives/index.php
+++ pages/archives/index.php
@@ -1,7 +1,7 @@
 <?php 
   // Does the dir has an index.html|php|htm file 
   function hasIndex($dir){
-
+    
     $fs = array_diff( scandir($dir), array(".", "..") );
     foreach($fs as $f){
       if(in_array($f, ["index.html", "index.php", "index.htm"])){
@@ -9,7 +9,7 @@
       }
     }
 
-    return false;
+    // return false;
     // old (glob) : 
     // foreach(glob($dir.'/index.{html,htm,php}',GLOB_BRACE) as $file){
     //   return basename($dir) . '/' . basename($file);
@@ -32,14 +32,14 @@
   $archivesdir = '../../archives';
   $currentdir = $archivesdir . $params;
   
-  // if current dir has index
-  $dir = new DirectoryIterator($currentdir);
-  
-  $index = hasIndex($dir);
+  $index = hasIndex($currentdir);
   if($index){
     // var_dump($index);
     header("Location: $index");
-  }
+  } 
+  
+  // if current dir has index
+  $dir = new DirectoryIterator($currentdir);
 
   $title = "ÉSAD·Pyrénées — Ateliers web — Archives";
   $section="archives";
@@ -53,60 +53,55 @@
 
   <main class="pane active" id="content">
     <nav>
-      
-        L’importation des archives est en cours ☺
-        <br><br>
+      L’importation des archives est en cours ☺<br><br>
 
-  <?php
+      <?php
+        $results = array();
+        
+        // browse currentdir, looking for subdirs or index
+        foreach ($dir as $fileinfo) {
+          if ($fileinfo->isDir() && !$fileinfo->isDot()) {
+            $index = hasIndex($fileinfo->getPathname());  
+              
+            $path = $fileinfo->getFilename() . '/';
 
+            if ($index != false ) {
+              $path = basename($dir) . '/' .$index;
+            }
+            
+            $dirArray = array(
+              'path'=>$path, 
+              'name'=>basename($dir)
+            );
 
-$results = array();
-
-
-
-// browse currentdir, looking for subdirs or index
-foreach ($dir as $fileinfo) {
-  if ($fileinfo->isDir() && !$fileinfo->isDot()) {
-    $index = hasIndex($fileinfo->getPathname());  
-      
-    $path = $fileinfo->getFilename() . '/';
-
-    if ($index != false ) $path = basename($dir) . '/' .$index;
-
-    
-    $dirArray = array(
-      'path'=>$path, 
-      'name'=>basename($dir)
-    );
-
-    $results[] = $dirArray;
-  }
-}
+            $results[] = $dirArray;
+          }
+        }
 
 
-echo "<ul>";
-if ($params) {
-  $up = dirname($currentdir);
-  $upname = basename($up);
-  echo "<li><a href='../'/>← $upname</a></li><li><br></li>";
-}
+        echo "<ul>";
+        if ($params) {
+          $up = dirname($currentdir);
+          $upname = basename($up);
+          echo "<li><a href='../'/>← $upname</a></li><li><br></li>";
+        }
 
-// display md contents if any
-$mdindex = hasMDIndex($currentdir);
-if($mdindex){
-  echo $Parsedown->text( file_get_contents( $mdindex ) );
-}
+        // display md contents if any
+        $mdindex = hasMDIndex($currentdir);
+        if($mdindex){
+          echo $Parsedown->text( file_get_contents( $mdindex ) );
+        }
 
-// sort dir results
-rsort($results);
-foreach ($results as $dir) {
-    echo "<li><a href='". $dir['path'] . "'>".$dir['name']."</a></li>";
-}
-echo "</ul>";
-?>
-</nav></main>
-<?php
-  include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php");
-?> 
+        // sort dir results
+        rsort($results);
+        foreach ($results as $dir) {
+            echo "<li><a href='". $dir['path'] . "'>".$dir['name']."</a></li>";
+        }
+        echo "</ul>";
+      ?>
+    </nav>
+  </main>
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php"); ?> 
 
   
