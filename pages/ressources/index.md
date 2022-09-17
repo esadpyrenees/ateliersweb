07f2c8539cd1fb62b9fac4c4814ae6ecb98eec3a
Julien Bidoret <julien@accentygrave.net>
2022-09-17 14:24:00 +0200

    Resources + references homepages

---
+++ pages/ressources/index.md
@@ -0,0 +1,8 @@
+# Ressources
+
+Ressources pédagogiques autour de HTML, CSS, FTP, Javascript, PHP, Python, la typographie, l’audio & la vidéo sur le web, le Responsive Web design, Canvas, CTRL Alt print (_web to print_), Kirby…
+
+<div class="nav-help">
+<strong><span class="nav-help-manicule nav-help-desktop">☜</span><span class="nav-help-manicule nav-help-mobile">☝</span></strong>
+Parcourir les sections <span class="nav-help-mobile">ci-dessus.</span>  <span class="nav-help-desktop">ci-contre.</span>
+</div>  
--- pages/ressources/index.php
+++ pages/ressources/index.php
@@ -1,14 +1,27 @@
-  <?php 
-    $title = "ÉSAD Pyrénées — Ateliers web — Ressources";
+<?php
+    // config
+    $title = "Ressources";
+    $description = "Ressources pédagogiques autour de HTML, CSS, FTP, Javascript, PHP, Python, Typographie, Audio & vidéo, Responsive, Canvas, CTRL Alt print, Kirby…";
     $section="ressources";
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php");
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php");
-  ?> 
+    $mdfile = "./index.md";
 
+    // includes
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtra.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php";
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php";
 
+    // nav snippet
+    if(isset($nav)) include_once $_SERVER["DOCUMENT_ROOT"] . $nav;
 
-  <?php 
-    include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php");
-  ?> 
+    // markdown!
+    $Parsedown = new ParsedownExtra();
 
-  
+?>
+
+    <main class="pane active" id="content">
+        <?= $Parsedown->text( file_get_contents( $mdfile ) ); ?>
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/date.php"); ?>
+    </main>
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php"); ?>
