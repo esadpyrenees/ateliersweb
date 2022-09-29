09f7834495fd27c30654eabb61100e8e6412ba36
Julien Bidoret <julien@accentygrave.net>
2022-09-29 02:08:57 +0200

    Reorder menu Lectures

--- snippets/lectures/lectures.php
+++ snippets/lectures/lectures.php
@@ -1,12 +1,12 @@
 <nav class="pane active">
   <ul>
+    <li><a href="#online">En ligne</a></li>
     <li><a href="#livres">Livres</a></li>
     <li><a href="#revues">Revues</a></li>
     <li><a href="#newsletters">Newsletters</a></li>
     <li><a href="#podcasts">Podcasts</a></li>
     <li><a href="#videos">Vidéos</a></li>
-    <li><a href="#sites">Sites</a></li>
+    <li><a href="#culturesnum">Culture numérique</a></li>
     <li><a href="#editeurs">Éditeurs</a></li>
-    <li><a href="#online">En ligne</a></li>
   </ul>
 </nav>
