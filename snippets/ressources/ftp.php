ddbdcf6f2f527a7b9087f76e2b1f21b69e85eb62
Julien Bidoret <julien@accentygrave.net>
2023-01-08 19:23:10 +0100

    FTP doc

--- snippets/ressources/ftp.php
+++ snippets/ressources/ftp.php
@@ -1,15 +1,13 @@
 <nav class="pane active">
   <ul>
-    <li class="<?= $subsubsection == 'ftp' ? 'opened' : '' ?>">
-        <a href="/web/pages/ressources/ftp/#ftp">FTP</a>        
-        <ul class="contextsub">
-            <li><a href="#pre-requis">Pré-requis</a></li>
-            <li><a href="#demarrer">Démarrer</a></li>
-            <li><a href="#clients">Logiciels FTP</a></li>
-            <li><a href="#structure">Structure</a></li>
-            <li><a href="#faq">FAQ</a></li>
-        </ul>
-    </li>
+    <li><a href="/web/pages/ressources/ftp/#ftp">FTP</a></li>
+    <li><a href="#pre-requis">Pré-requis</a></li>
+    <li><a href="#demarrer">Démarrer</a></li>
+    <li><a href="#clients">Logiciels FTP</a></li>
+    <li><a href="#structure">Structure</a></li>
+    <li><a href="#faq">FAQ</a></li>
+  </ul>
+  <ul>
     <li  class="<?= $subsubsection == 'alternatives' ? 'opened' : '' ?>"><a href="/web/pages/ressources/ftp/alternatives">Alternatives</a></li>
   </ul>
 </nav>
