d68aea287ee04de5db8d5391fcf35fad8feffb8d
Julien Bidoret <julien@accentygrave.net>
2023-02-07 18:48:58 +0100

    On strike

--- snippets/footer.php
+++ snippets/footer.php
@@ -1,4 +1,12 @@
 
+  <div id="strike" style="position:fixed; background:black; inset:0; display:flex; justify-content:center; align-items:center; color:white">
+    <p>
+    Contre la destruction de notre modèle social.<br>
+    Contre l’augmentation croissante des inégalités.<br>
+    Contre la stigmatisation systématique des plus fragiles.<br>
+    Ce site est en grève.
+    </p>
+  </div>
   <script src="/web/assets/js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="/web/assets/js/notes.js"></script>
   <script src="/web/assets/js/main.js"></script>
