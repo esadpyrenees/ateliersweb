94471db09105d8385e72168bcd66669151ca0e69
Julien Bidoret <julien@accentygrave.net>
2023-02-15 18:11:41 +0100

    Strike over

--- snippets/footer.php
+++ snippets/footer.php
@@ -1,5 +1,5 @@
 
-  <div id="alarme" style="display:none; position:fixed; background:black; inset:0; display:flex; justify-content:center; align-items:center; color:white; padding:1em">
+  <!-- <div id="alarme" style="position:fixed; background:black; inset:0; display:flex; justify-content:center; align-items:center; color:white; padding:1em; ">
     <p>
     Contre la destruction de notre modèle social.<br>
     Contre l’augmentation croissante des inégalités.<br>
@@ -7,7 +7,7 @@
     Ce site est en grève.<br><br>
     <a href="#rejoignons-les-luttes" >✊</a>
     </p>
-  </div>
+  </div> -->
   <script src="/web/assets/js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="/web/assets/js/notes.js"></script>
   <script src="/web/assets/js/main.js"></script>
@@ -16,13 +16,14 @@
   <?php endif ?>
 
   <script>
-    function strike(e){
-      e.preventDefault()
-      let alarme = document.querySelector('#alarme');
-      alarme.parentElement.removeChild(alarme);
-    }
-    let raisedfist = document.querySelector('a[href="#rejoignons-les-luttes"]');
-    raisedfist.onclick = strike;
+    // function strike(e){
+    //   e.preventDefault()
+    //   let alarme = document.querySelector('#alarme');
+    //   alarme.parentElement.removeChild(alarme);
+    // }
+    // let raisedfist = document.querySelector('a[href="#rejoignons-les-luttes"]');
+    // raisedfist.onclick = strike;
+    // raisedfist.click();
 
   </script>
 
