20de48315d8df16c234f5eeca69289ccaf99c8af
Julien Bidoret <julien@accentygrave.net>
2023-03-02 23:34:39 +0100

    Grève Internationale Trans★Féministe — Décroissance Numérique

--- snippets/footer.php
+++ snippets/footer.php
@@ -1,5 +1,5 @@
 
-  <div id="alarme" style="position:fixed; background:black; inset:0; display:flex; justify-content:center; align-items:center; color:white; padding:1em; ">
+  <!-- <div id="alarme" style="position:fixed; background:black; inset:0; display:flex; justify-content:center; align-items:center; color:white; padding:1em; ">
     <p>
     Contre la destruction de notre modèle social.<br>
     Contre l’augmentation croissante des inégalités.<br>
@@ -7,7 +7,7 @@
     Contre le <i>Cloud</i> et son monde, ce site est en grève.<br><br>
     <a href="#rejoignons-les-luttes" style="font-size:xx-large">✊</a>
     </p>
-  </div>
+  </div> -->
   <script src="/web/assets/js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="/web/assets/js/notes.js"></script>
   <script src="/web/assets/js/main.js"></script>
