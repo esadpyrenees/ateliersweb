dd81ebae44b025959d8e6dd3fc2547d769d4334e
Julien Bidoret <julien@accentgrave.net>
2023-09-05 20:32:18 +0200

    A better template (with parsedownextra plugin)

--- pages/_template/index.php
+++ pages/_template/index.php
@@ -11,8 +11,9 @@
     $custom_js = "custom.js"; // relative or absolute file URL
 
     // includes
-    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php';
-    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtra.php';
+    require $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php';
+    require $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtra.php';
+    require $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtraPlugin.php';
     include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php";
     include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php";
 
@@ -20,7 +21,8 @@
     if(isset($nav)) include_once $_SERVER["DOCUMENT_ROOT"] . $nav;
 
     // markdown!
-    $Parsedown = new ParsedownExtra();
+    $Parsedown = new ParsedownExtraPlugin();
+    $Parsedown->figuresEnabled = true;
 
 ?>
 
