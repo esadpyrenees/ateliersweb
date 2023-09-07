5f8ef8fa1a7a19097a222aa3d6b780da0e29f770
Julien Bidoret <julien@accentgrave.net>
2023-09-07 18:57:31 +0200

    Copy code tool

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,7 +135,33 @@ headings.forEach(function(heading){
 })
 
 
+var copy = function(target) {
+    var textArea = document.createElement('textarea')
+    textArea.setAttribute('style','width:1px;border:0;opacity:0;')
+    document.body.appendChild(textArea)
+    textArea.value = target.textContent
+    textArea.select()
+    document.execCommand('copy')
+    document.body.removeChild(textArea)
+}
 
+var pres = document.querySelectorAll("main pre")
+pres.forEach(function(pre){
+  var button = document.createElement("button")
+  button.className = "copybtn"
+  button.innerHTML = `<svg aria-hidden="true" class="octicon octicon-clippy" height="16" viewBox="0 0 14 16" width="14">
+      <path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path>
+  </svg>`;
+  pre.insertAdjacentElement('beforeend', button)
+  button.addEventListener('click', function(e){
+    e.preventDefault()
+    button.classList.add("copied");
+    setTimeout(() => {
+        button.classList.remove("copied");
+    }, 1500);
+    copy(pre.childNodes[0])
+  })
+})
 
 // var debug = document.createElement('div');
 // debug.id="debug";
