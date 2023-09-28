e1252907e340c93875c61b366ee247ff09c406eb
Julien Bidoret <julien@accentgrave.net>
2023-09-28 08:06:21 +0200

    GoPro 2023-2024: groups on click

--- pages/gopro/2324/expectations/index.php
+++ pages/gopro/2324/expectations/index.php
@@ -35,5 +35,41 @@
     </main>
 
     <!-- <script src="arpentage.js"></script> -->
+    <script>
+        const liste = [
+        "Marjorie Biauné",
+        "Solange Caillon",
+        "Ryan Cracco",
+        "Maëva Huber",
+        "Morgane Lazarus",
+        "Yuyuan Ma",
+        "Mélina Maulet",
+        "Émilie Roudaut",
+        "Izis Saliba",
+        "Kassandra Vanmansart",
+        "Sarah Bonnemazou", 
+        "Anita Brémond",
+        "Maxime Carle",
+        "Coralie Escaich",
+        "Amandine Lavedan",
+        "Coline Malaman",
+        "Morganne Marion",
+        "Emma Martin",
+        "Alice Mourey",
+        "Guilhem Pujol",
+        "Aurore Tajan"
+        ]
+
+        document.onclick = () => {
+            for (let i = liste.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                [liste[i], liste[j]] = [liste[j], liste[i]];
+            }
+
+            let randomized = liste.reduce((liste, item, idx) => (liste[idx / 3 | 0] ??= []).push(item) && liste, []);
+
+            console.log(JSON.stringify(randomized));
+        }
+    </script>
 
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php"); ?>
