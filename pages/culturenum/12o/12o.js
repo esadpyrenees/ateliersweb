901c6316c08903e46b33621fa9d3c54616f28bf7
Julien Bidoret <julien@accentgrave.net>
2023-10-12 01:27:50 +0200

    12o

---
+++ pages/culturenum/12o/12o.js
@@ -0,0 +1,38 @@
+var urls = ["ANTIFA-INFRA.svg",
+"BOLT.svg",
+"CLOUD-NO-THANKS.svg",
+"sticker_WhatCloudGoWrong_96dpi.png",
+"agile.gif",
+"almost.gif",
+"antifa.gif",
+"cloudabolition.gif",
+"cloudsoption.gif",
+"counter.png",
+"countercloud.png",
+"dancepicket.gif",
+"depletion.gif",
+"elastic.gif",
+"feral.gif",
+"imagination-slipping-into-ever-12-2-2022.png",
+"IN-THE-RUINS-OF-BIG-TECH-12-1-2022.png",
+"less.gif",
+"queeringoperations.gif",
+"radicals.png",
+"regeneration.png",
+"ruins.png",
+"scale.png",
+"showmethebudget.png",
+"slipping.png",
+"strike.png",
+"take.gif",
+"tech.gif",
+"wicked.png",
+"counter_cloud_action.svg"];
+
+var sticker = document.querySelector('#sticker');
+
+setInterval(() => {
+  sticker.src = "../8m/" + urls[Math.floor( Math.random() * urls.length) ]
+}, 2000);
+
+
---
+++ pages/culturenum/12o/index.php
@@ -0,0 +1,50 @@
+<?php
+    // config
+
+    $title = "Grève Internationale Trans★Féministe — Décroissance Numérique";
+    $section="culturenum";
+    $subsection="12o";
+    $mdfile = "./12o.md";
+    // $date = "today";
+
+    // includes
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/Parsedown.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/web/_inc/ParsedownExtra.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/header.php";
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/web/snippets/nav.php";
+
+    // nav snippet
+    if(isset($nav)) include_once $_SERVER["DOCUMENT_ROOT"] . $nav;
+
+    // markdown!
+    $Parsedown = new ParsedownExtra();
+
+?>
+
+<style>
+    .main8m {
+        width: calc(100vw - 3em);
+    }
+    iframe {
+        height: calc(100vh - 6em);
+        margin: 3em 0;
+    }
+    #sticker {
+        position: fixed;
+        bottom:3em;
+        right:3em;
+        transform:rotate(-9deg);
+        width: 350px;
+        max-width: 25vw;
+    }
+</style>
+
+    <main class="pane active typeset main8m" id="content">
+        <?= $Parsedown->text( file_get_contents( $mdfile ) ); ?>
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/date.php"); ?>
+    </main>
+
+    <img src="../8m/CLOUD-NO-THANKS.svg" id="sticker">
+
+    <script src="12o.js"></script>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/web/snippets/footer.php"); ?>
