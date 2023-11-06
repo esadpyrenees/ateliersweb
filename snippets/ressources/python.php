4cbec705decf069a3e2a140b481d396334a311d7
Julien Bidoret <julien@accentgrave.net>
2023-11-06 17:07:11 +0100

    Moved Mkdocs

--- snippets/ressources/python.php
+++ snippets/ressources/python.php
@@ -8,21 +8,7 @@
     <li class="<?= $subsubsection == 'files' ? 'opened' : '' ?>"><a href="/web/pages/ressources/python/files/">Fichiers</a> </li>
   </ul>
   <ul>
-    <li class="<?= $subsubsection == 'mkdocs' ? 'opened' : '' ?>"><a href="/web/pages/ressources/python/mkdocs/">MkDocs</a> </li>
-    <?php if($subsubsection == 'mkdocs'): ?>
-      <ul>
-        <li><a href="#documentation">Documentation</a></li>
-        <li><a href="#environnement-virtuel">Environnement</a></li>
-        <li><a href="#structure-du-projet">Structure </a></li>
-        <li><a href="#configuration">Configuration</a></li>
-        <li><a href="#commandes">Commandes</a></li>
-        <li><a href="#contenu">Contenu</a></li>
-        <li><a href="#demo">Démo</a></li>
-        <li><a href="#template">Templates</a></li>
-        <li><a href="#hack">Hack</a></li>
-        <li><a href="#scraping">Scraping</a></li>
-        <li><a href="#publier">Publier</a></li>
-      </ul>
-    <?php endif ?>
+    <li><a href="/web/pages/ressources/mkdocs/">MkDocs</a> </li>
+    
   </ul>
 </nav>
