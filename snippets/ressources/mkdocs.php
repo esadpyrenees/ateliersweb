a0e0d02637408746a50db336d42e85c6bafe6668
Julien Bidoret <julien@accentgrave.net>
2023-11-06 16:16:44 +0100

    Moved Mkdocs

---
+++ snippets/ressources/mkdocs.php
@@ -0,0 +1,16 @@
+<nav class="pane active">
+
+      <ul>
+        <li><a href="#documentation">Documentation</a></li>
+        <li><a href="#environnement-virtuel">Environnement</a></li>
+        <li><a href="#structure-du-projet">Structure </a></li>
+        <li><a href="#configuration">Configuration</a></li>
+        <li><a href="#commandes">Commandes</a></li>
+        <li><a href="#contenu">Contenu</a></li>
+        <li><a href="#demo">Démo</a></li>
+        <li><a href="#template">Templates</a></li>
+        <li><a href="#hack">Hack</a></li>
+        <li><a href="#scraping">Scraping</a></li>
+        <li><a href="#publier">Publier</a></li>
+      </ul>
+</nav>
