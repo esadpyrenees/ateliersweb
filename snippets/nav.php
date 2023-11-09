ff7a1c6f5401787433ad6d72f085f6c73d977c35
Julien Bidoret <julien@accentgrave.net>
2023-11-09 03:37:54 +0100

    Unused nav panel

--- snippets/nav.php
+++ snippets/nav.php
@@ -55,19 +55,6 @@
     </ul>
 </nav>
 
-<!-- <nav class="pane subnav <?php echo $section == 'references' ? 'active' : '' ?>"  id="references">
-    <ul>
-        <li class="<?php echo $subsection == 'htmlcss' ? 'opened' : '' ?>"><a href="/web/pages/references/htmlcss/">HTML / CSS</a></li>
-        <li class="<?php echo $subsection == 'typo' ? 'opened' : '' ?>"><a href="/web/pages/references/typo/">Typographie</a></li>
-        <li class="<?php echo $subsection == 'foundries' ? 'opened' : '' ?>"><a href="/web/pages/references/foundries/">Fonderies</a></li> 
-        <li class="<?php echo $subsection == 'ecrituresnumeriques' ? 'opened' : '' ?>"><a href="/web/pages/references/ecrituresnumeriques/">Écritures numériques</a></li>
-        <li class="<?php echo $subsection == 'webdocumentaire' ? 'opened' : '' ?>"><a href="/web/pages/references/webdocumentaire/">Webdoc</a></li> 
-        <li class="<?php echo $subsection == 'visite' ? 'opened' : '' ?>"><a href="/web/pages/references/visite/">Visite</a></li>        
-        <li class="<?php echo $subsection == 'histoire' ? 'opened' : '' ?>"><a href="/web/pages/references/histoire/">Histoire</a></li>
-        <li class="<?php echo $subsection == 'gopro' ? 'opened' : '' ?>"><a href="/web/pages/references/gopro/">Go pro !</a></li>
-    </ul>
-</nav> -->
-
 <nav class="pane subnav <?php echo $section == 'gopro' ? 'active' : '' ?>"  id="gopro">
     <ul>
         <li><a href="/web/pages/gopro/#syndicats">✊</a></li>
