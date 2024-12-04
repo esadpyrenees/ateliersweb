f968bc8fc93901cd5355c3e272a8f277c18f4949
Julien Bidoret <julien@accentgrave.net>
2024-12-04 18:13:16 +0100

    Home to beta

--- pages/home.php
+++ pages/home.php
@@ -13,16 +13,21 @@
 ?>
   <section class="pane home" id="content">
 
-    <blockquote cite="http://luckysoap.com/statements/handmadeweb.html" class="hidden">
-      <!-- <strong>In today’s highly commercialized web of <span>multinational corporations</span>, <span>proprietary applications</span>, <span>read-only devices</span>, <span>search algorithms</span>, <span>Content Management Systems</span>, <span>WYSIWYG editors</span>, and <span>digital publishers</span>, it becomes an <span class="rad">increasingly radical act</span> to <span>hand-code</span> and <span>self-publish</span> <span>experimental web art</span> and <span>writing projects</span>.</strong> <cite>J. R. Carpenter, <br><a href="http://luckysoap.com/statements/handmadeweb.html">A handmade web</a>.</cite>     -->
-      
-      <strong>In<span> </span>today’s<span> </span>highly<span> </span>commercialized<span> </span>web<span> </span>of<span> </span>multinational<span> </span>corporations,<span> </span>proprietary<span> </span>applications,<span> </span>read-only<span> </span>devices,<span> </span>search<span> </span>algorithms,<span> </span>Content<span> </span>Management<span> </span>Systems,<span> </span>WYSIWYG<span> </span>editors,<span> </span>and<span> </span>digital<span> </span>publishers,<span> </span>it<span> </span>becomes<span> </span>an<span></sp an><span<span> </span>increasingly<span> </span>radical<span> </span>act<span> </span>to<span> </span>hand-code<span> </span>and<span> </span>self-publish<span> </span>experimental<span> </span>web<span> </span>art<span> </span>and<span> </span>writing<span> </span>projects.</strong> <cite>J. R. Carpenter, <br><a href="http://luckysoap.com/statements/handmadeweb.html">A handmade web</a>.</cite>    
+    <p class="handmake">
+      <span>radical</span>
+      <span>web</span>
+      <span>design</span>
+    </p>
+    <a href="https://beta.radicalweb.design">
+      <img src="/web/assets/img/parici.png" alt="par ici">
+    </a>
 
+    <!-- <blockquote cite="http://luckysoap.com/statements/handmadeweb.html" class="hidden"> -->
+      <!-- <strong>In today’s highly commercialized web of <span>multinational corporations</span>, <span>proprietary applications</span>, <span>read-only devices</span>, <span>search algorithms</span>, <span>Content Management Systems</span>, <span>WYSIWYG editors</span>, and <span>digital publishers</span>, it becomes an <span class="rad">increasingly radical act</span> to <span>hand-code</span> and <span>self-publish</span> <span>experimental web art</span> and <span>writing projects</span>.</strong> <cite>J. R. Carpenter, <br><a href="http://luckysoap.com/statements/handmadeweb.html">A handmade web</a>.</cite>     -->     
+      <!-- <strong>In<span> </span>today’s<span> </span>highly<span> </span>commercialized<span> </span>web<span> </span>of<span> </span>multinational<span> </span>corporations,<span> </span>proprietary<span> </span>applications,<span> </span>read-only<span> </span>devices,<span> </span>search<span> </span>algorithms,<span> </span>Content<span> </span>Management<span> </span>Systems,<span> </span>WYSIWYG<span> </span>editors,<span> </span>and<span> </span>digital<span> </span>publishers,<span> </span>it<span> </span>becomes<span> </span>an<span></sp an><span<span> </span>increasingly<span> </span>radical<span> </span>act<span> </span>to<span> </span>hand-code<span> </span>and<span> </span>self-publish<span> </span>experimental<span> </span>web<span> </span>art<span> </span>and<span> </span>writing<span> </span>projects.</strong> <cite>J. R. Carpenter, <br><a href="http://luckysoap.com/statements/handmadeweb.html">A handmade web</a>.</cite>     -->
       <!-- Le<span> </span>8<span> </span>mars<span> </span>2024,<span> </span>nous<span> </span>appelons<span> </span>à<span> </span>une<span> </span>journée<span> </span>d’action<span> </span>contre<span> </span>le<span> </span>cloud<span> </span>—<span> </span>pour<span> </span>un<span> </span>cessez-le-feu<span> </span>permanent<span> </span>en<span> </span>Palestine<span> </span>et<span> </span>pour<span> </span>la<span> </span>fin<span> </span>du<span> </span>génocide<span> </span>facilité<span> </span>par<span> </span>le<span> </span>complexe<span> </span>militaro-technologique<span> </span>du<span> </span>gouvernement<span> </span>israélien. <cite><a href="https://titipi.org/8m/">8M</a></cite> -->
-    </blockquote>
+    <!-- </blockquote> -->
 
-    
-    
     <!-- Comments are free, facts are sacred -->
     
     <!-- <article class="strike"><p style="margin:0">
@@ -37,13 +42,9 @@
     <a href="https://ecolesartdesignenlutte.fr/" id="esadsenlutte">
     <img id="esadsenlutte" src="/web/assets/img/afaaa835-cb23-4f50-adbd-d4a20baa59a4.jpeg" alt="Mobilisation nationale — lundi 13/03 — écoles art, design archi en lutte — https://ecolesartdesignenlutte.fr/">
     </a> 
-
-    
-
     <article class="visite" >
       La visite du web, c’est <a href="pages/references/visite" style="display: inline-block;text-decoration: none;padding: .15em 0 .1em .5em ;position: relative;z-index: 0;background: url('data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJDYWxxdWVfMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgeD0iMHB4IiB5PSIwcHgiCgkgdmlld0JveD0iMCAwIDExOCAzNCIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgMTE4IDM0OyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+CjxwYXRoIGQ9Ik0xMTYuMSwxNC45bC0xNC0xNEMxMDEuNSwwLjMsMTAwLjgsMCwxMDAsMHYwbDAsMEgwdjM0aDEwMGwwLDB2MGMwLjgsMCwxLjUtMC4zLDIuMS0wLjlsMTQtMTQKCUMxMTcuMywxOCwxMTcuMywxNiwxMTYuMSwxNC45eiBNMTAwLDMxSDNWM2g5N2wwLDBjMCwwLDAsMCwwLDB2MGgwbDE0LDE0TDEwMCwzMXoiLz4KPC9zdmc+Cg==');background-size: auto;background-size: auto 100%; ">par ici !<span style="width: 1.15em;height: 100%;position: absolute;right: -1em;top: 0;z-index: 1;background: url('data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJDYWxxdWVfMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgeD0iMHB4IiB5PSIwcHgiCgkgdmlld0JveD0iMCAwIDExOCAzNCIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgMTE4IDM0OyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+CjxwYXRoIGQ9Ik0xMTYuMSwxNC45bC0xNC0xNEMxMDEuNSwwLjMsMTAwLjgsMCwxMDAsMHYwbDAsMEgwdjM0aDEwMGwwLDB2MGMwLjgsMCwxLjUtMC4zLDIuMS0wLjlsMTQtMTQKCUMxMTcuMywxOCwxMTcuMywxNiwxMTYuMSwxNC45eiBNMTAwLDMxSDNWM2g5N2wwLDBjMCwwLDAsMCwwLDB2MGgwbDE0LDE0TDEwMCwzMXoiLz4KPC9zdmc+Cg==');background-size: auto 100%;background-position: 100% 0;"></span></a>
     </article>
-
     -->
 
   </section>
